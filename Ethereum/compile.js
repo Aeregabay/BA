0234810d3502229fdcb837198e3650d6bbce223e
Arik Gabay <a.gabay94@gmail.com>
2018-12-07 16:33:41 +0100

    delete cache git

--- Ethereum/compile.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-const fs = require("fs");
-const solc = require("solc");
-
-const tradingPath = path.resolve(__dirname, "contracts", "TradingContract.sol");
-const source = fs.readFileSync(tradingPath, "utf8");
-
-module.exports = solc.compile(source, 1).contracts[":TradingContract"];
